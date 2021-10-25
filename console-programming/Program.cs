8dc733920b75975e5ef2893652cb911b98e1400a
Diren Gündoğdu <DirenGundogdu@users.noreply.github.com>
2021-10-25 18:09:16 +0300

    first commit

---
+++ console-programming/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Surname");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name + " " + surname);
+        }
+    }
+}
